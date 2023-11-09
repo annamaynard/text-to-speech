c1e159258ca84ab6a53a373e2f4dd624c1598502
annamaynard <anna.maynard@gmail.com>
2023-11-09 13:40:09 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
+import {useState} from 'react'
+import Header from './components/Header';
+import Section from './components/Section';
 import './App.css';
 
 function App() {
+    const [text, setText] =useState('');
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+        <Header />
+        <Section text={text} setText={setText}/>
     </div>
   );
 }
---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+
+const Header = ()  =>  {
+    const logo = "./logo.png"
+
+    return (
+        <img src = {logo} alt="logo" />
+     )
+}
+export default Header; 
