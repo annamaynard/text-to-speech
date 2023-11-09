33f1d2b3f91db2cd2458be0f83d47d7859f89cef
annamaynard <anna.maynard@gmail.com>
2023-11-09 14:21:20 -0500

    sucessful call to api

---
+++ src/components/AWSConfig.js
@@ -0,0 +1,8 @@
+const AWSConfig = {
+    accessKeyId: 'AKIAXUR6OHLI6UITYIQH',
+    secretAccessKey: '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',
+    region: 'us-east-1'
+  }
+  
+  export default AWSConfig;
+  
--- src/components/Section.jsx
+++ src/components/Section.jsx
@@ -1,15 +1,57 @@
+import React, { useState, useEffect } from 'react';
+import AWS from 'aws-sdk';
 import '../App.css';
+import AWSConfig from './AWSConfig';
+;
 
-const Section =({text, setText}) => {
-    return(
-        <div className="section-container">
-            <textarea
-             value={text}
-             onChange={(e) => setText(e.target.value)} />
-            <button
-            className="btn-convert">Convert to Speech</button>
-        </div>
+const Section = ({ text, setText }) => {
+  const [audioUrl, setAudioUrl] = useState(null);
 
-    )
-}
+  useEffect(() => {
+    if (audioUrl) {
+      const audio = new Audio(audioUrl);
+      audio.play();
+    }
+  }, [audioUrl]);
+
+  const convertToSpeech = () => {
+    AWS.config.update(AWSConfig); // Set AWS Config
+
+    const Polly = new AWS.Polly();
+
+    const params = {
+      OutputFormat: 'mp3',
+      Text: text,
+      VoiceId: 'Joanna'
+    };
+
+    Polly.synthesizeSpeech(params, (err, data) => {
+      if (err) {
+        console.error(err);
+      } else {
+        const audioBlob = new Blob([data.AudioStream], { type: 'audio/mpeg' });
+        const audioUrl = URL.createObjectURL(audioBlob);
+        setAudioUrl(audioUrl);
+      }
+    });
+  };
+
+  return (
+    <div className="section-container">
+      <textarea
+        value={text}
+        onChange={(e) => setText(e.target.value)}
+      />
+      <button
+        className="btn-convert"
+        onClick={convertToSpeech}
+      >
+        Convert to Speech
+      </button>
+
+      {audioUrl && <audio controls src={audioUrl} />}
+    </div>
+  );
+};
+
 export default Section;
